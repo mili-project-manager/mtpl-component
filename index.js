03fd84fa15009b019f22aea5627ab9ea60986693
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-10-21 21:08:56 +0800

    fix(src): files in src was covered

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ exports.engines = ">=1.0.0<2.0.0"
 
 exports.rules = [
   {
+    path: 'src',
+    upgrade: 'keep',
+  },
+  {
     path: 'package.json.mustache',
     upgrade: 'merge',
     handlers: ['mustache']

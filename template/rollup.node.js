b9f2150c12591e7d69e92d6e275afefd5cc2cfef
Val.istar.Guo <val.istar.guo@gmail.com>
2019-09-08 14:10:06 +0800

    fix: alias in .d.ts isn't replaced

--- template/rollup.node.js
+++ template/rollup.node.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
-import tsPath from 'rollup-plugin-ts-paths'
 import json from 'rollup-plugin-json'
 import autoExternal from 'rollup-plugin-auto-external'
+import ttypescript from 'ttypescript'
 
 const warnFilter = /The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten/
 
@@ -12,7 +12,14 @@ const onwarn = (warning, warn) => {
   warn(warning)
 }
 
-const plugins = [autoExternal(), tsPath(), json(), typescript()]
+const plugins = [
+  autoExternal(),
+  json(),
+  typescript({
+    clean: true,
+    typescript: ttypescript,
+  }),
+]
 
 const sourcemap = process.env.NODE_ENV === 'development'
 
